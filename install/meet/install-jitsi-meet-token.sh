030484597f65c29a198e9da42f502ea204b092a8
Keun-yang Son <ky.son@kt.com>
2020-10-08 17:57:51 +0900

    디렉토리 생성에 -p 옵션 추가.

--- install/meet/install-jitsi-meet-token.sh
+++ install/meet/install-jitsi-meet-token.sh
@@ -31,7 +31,7 @@ printf "\e[00;32m-------------------- Install LUACRYPTO ---------------------\e[
 sudo luarocks install luacrypto
 
 printf "\e[00;32m---------------------- Install CJSON -----------------------\e[00m\n"
-mkdir $HOME/src
+mkdir -p $HOME/src
 cd $HOME/src
 
 sudo luarocks download lua-cjson

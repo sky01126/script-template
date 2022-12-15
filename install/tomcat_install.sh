02dfbf6c7ad4526ffdce3f2d60b71b714976cd26
sky01126 <GIByang@1216>
2022-12-15 23:26:43 +0900

    2022-12-15 #7

--- install/tomcat_install.sh
+++ install/tomcat_install.sh
@@ -705,7 +705,6 @@ PRGDIR=\$(dirname \"\${PRG}\")
 source \${PRGDIR}/config.sh
 
 # ---------------------------------------------------------------------------------
-export HOSTNAME=\$(ostname)
 # export SERVER_PUBLIC_IP=\$(ifconfig -a | grep \"inet \" | grep \"Bcast:\" | awk '{print \$2}' | awk -F: '{print \$2}' | grep '^211')
 # export SERVER_PRIVATE_IP=\$(ifconfig -a | grep \"inet \" | grep \"Bcast:\" | awk '{print \$2}' | awk -F: '{print \$2}' | grep '^10')
 
